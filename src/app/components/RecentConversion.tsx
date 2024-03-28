f9ad0e1164573643e615f4a3eb3f53c5f3ffc6f5
YawOtuo <yotuo2003@gmail.com>
2024-03-28 09:54:39 +0000

    previous design

--- src/app/components/RecentConversion.tsx
+++ src/app/components/RecentConversion.tsx
@@ -5,8 +5,9 @@ function RecentConversions() {
   const { prev } = useStoreInLC();
   return (
     <div className="flex flex-col">
-      <p className="font-semibold pb-5">Previous Conversions</p>
-
+      {prev?.length > 0 && (
+        <p className="font-semibold pb-5">Previous Conversions</p>
+      )}
       <div className="grid grid-cols-1 md:grid-cols-3 gap-5 gap-y-5">
         {prev?.map((r: any, index: any) => (
           <div className="flex flex-wrap" key={index}>
